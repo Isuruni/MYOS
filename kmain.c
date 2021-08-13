5557e0a3445603a8766c7f97ab1c8032c1fcf1ce
Isuruni Rathnayaka <rathnaya-se18038@stu.kln.ac.lk>
2021-08-13 18:34:32 +0530

    kmain.c added to integrate_segmentation
    Contain kmain function

--- kmain.c
+++ kmain.c
@@ -1,12 +1,12 @@
 #include "framebuffer.h"
 #include "serial_port.h"
-
+#include "segmentation.h"
 void kmain(){
 
     char msg[] = "MYOS";
    
-    
-    serial_write(0x3F8, msg, 4);
-    fb_write(msg, 4);
-    
+   serial_write(0x3F8, msg, 4);
+   fb_write(msg, 4);
+   segments_install_gdt();
+   
 }
