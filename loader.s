60f218ab787ab638d53e0ea4c7fbb43aee96aef0
Isuruni Rathnayaka <rathnaya-se18038@stu.kln.ac.lk>
2021-08-27 13:11:15 +0530

    Updated loader.s added to integrate_user_modes

--- loader.s
+++ loader.s
@@ -1,32 +1,36 @@
- global loader                      ; 
+global loader                                              ; the entry symbol for ELF
 
-    MAGIC_NUMBER equ 0x1BADB002     ; 
-    				     ;	
-    FLAGS        equ 0x0            ; 
-    CHECKSUM     equ -MAGIC_NUMBER  ; 
-                                    ; 
- KERNEL_STACK_SIZE equ 4096         ; 
+extern kmain                                               ; function kmain is defined elsewhere
 
-    section .bss
-    align 4                         ; 
-    kernel_stack:                   ; 
-        resb KERNEL_STACK_SIZE      ;  
+MAGIC_NUMBER    equ 0x1BADB002                             ; define the magic number constant
+ALIGN_MODULES   equ 0x00000001                             ; tell GRUB to align modules
 
-    section .text                  ; 
-    align 4                         ; 
-        dd MAGIC_NUMBER             ; 
-        dd FLAGS                    ; 
-        dd CHECKSUM                 ; 
+                                                           ; calculate the checksum (all options + checksum should equal 0)
+CHECKSUM        equ -(MAGIC_NUMBER + ALIGN_MODULES )       
 
-    loader:                         ; 
-    	
-        mov esp, kernel_stack + KERNEL_STACK_SIZE; 
-                                                 ; 
-    						   ; 
+
+KERNEL_STACK_SIZE equ 4096                                 ; size of stack in bytes
+
+section .bss
+align 4                                                    ; align at 4 bytes
+kernel_stack:                                              ; label points to beginning of memory
+    resb KERNEL_STACK_SIZE                                 ; reserve stack for the kernel
+
+section .text                                              ; start of the text (code) section
+align 4                                                    ; the code must be 4 byte aligned
+    dd MAGIC_NUMBER                                        ; write the magic number to the machine code,
+    dd ALIGN_MODULES                                       ;
+    dd CHECKSUM                                            ; and the checksum
+
+
+loader:                                                    ; the loader label (defined as entry point in linker script)
+    mov esp, kernel_stack + KERNEL_STACK_SIZE              ; point esp to the start of the
+                                                           ; stack (end of memory area)
     
-    extern kmain                            ; 
+    push ebx                                               ; multiboot info in ebx 
     
-    call kmain                                  ; 
-    
-    .loop:
-jmp .loop                   ; 
+    call kmain                                             ; Calling the function kmain
+   
+.loop:
+    jmp .loop                                              ; loop forever
+
