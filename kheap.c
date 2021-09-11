e2f3ab9e9a59e0a85f0097b34ea375073a6a74f0
Isuruni Rathnayaka <rathnaya-se18038@stu.kln.ac.lk>
2021-09-11 11:59:53 +0530

    Updated kheap.c

--- kheap.c
+++ kheap.c
@@ -1,20 +1,38 @@
 #include "kheap.h"
 
-// kernel_end is defined in the linker script.
-extern u32int kernel_end;
-u32int placement_address = (u32int)&kernel_end;
+u32int g_KerNelPhysicalAddressStart = 0;
+u32int g_CurrentPhysicalAddressTop = 0;
 
-u32int kmalloc(u32int size) {
-	u32int mem =  placement_address;
-	placement_address += size;
-	return mem;
+void set_physical_address(u32int kerNelPhysicalStart,
+                          u32int kernelPhysicalEnd) {
+  g_KerNelPhysicalAddressStart = kerNelPhysicalStart;
+  g_CurrentPhysicalAddressTop = kernelPhysicalEnd;
 }
 
-u32int kmalloc_page() {
-	if (placement_address & 0xFFFFF000) { // not alligned 
-        	// Align the placement address;
-		placement_address &= 0xFFFFF000;
-		placement_address += 0x1000;
-    	}
-	return kmalloc(0x1000);
+u32int kmalloc_int(u32int size, u32int align, u32int *pAddrPtr) {
+  if (align == 1) {
+    if (g_CurrentPhysicalAddressTop & 0x00000FFF) {
+      // Align the placement address;
+      g_CurrentPhysicalAddressTop &= 0xFFFFF000;
+      g_CurrentPhysicalAddressTop += 0x1000;
+    }
+  }
+  if (pAddrPtr) {
+    *pAddrPtr = g_CurrentPhysicalAddressTop;
+  }
+  u32int tmp = g_CurrentPhysicalAddressTop;
+  g_CurrentPhysicalAddressTop += size;
+  return tmp;
 }
+
+u32int kmalloc_a(u32int size) { return kmalloc_int(size, 1, 0); }
+
+u32int kmalloc_p(u32int size, u32int *pAddrPtr) {
+  return kmalloc_int(size, 0, pAddrPtr);
+}
+
+u32int kmalloc_ap(u32int size, u32int *pAddrPtr) {
+  return kmalloc_int(size, 1, pAddrPtr);
+}
+
+u32int kmalloc(u32int size) { return kmalloc_int(size, 0, 0); }
