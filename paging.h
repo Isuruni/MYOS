13fc39a2da78fdf731eb9a74b292303ce528bf57
Isuruni Rathnayaka <rathnaya-se18038@stu.kln.ac.lk>
2021-09-11 12:18:13 +0530

    Updated paging.h

--- paging.h
+++ paging.h
@@ -1,60 +1,99 @@
-#ifndef PAGING_H
-#define PAGING_H
+#ifndef INCLUDE_PAGING_H
+#define INCLUDE_PAGING_H
 
-#define NUM_PAGES 1024
-#define PAGE_FRAME_SIZE 4096
 
-#define PRESENT         1
-#define PAGE_READONLY   0
-#define PAGE_READWRITE  1
-#define PAGE_USER       1
-#define PAGE_KERNEL     0
-#define PAGE_SIZE_4KB   0
-#define PAGE_SIZE_4MB   1
+#pragma once
+/* Typedefs, to standardise sizes across platforms.
+ * These typedefs are written for 32-bit X86.
+ */
+typedef unsigned int u32int;
+typedef int s32int;
+typedef unsigned short u16int;
+typedef short s16int;
+typedef unsigned char u8int;
+typedef char s8int;
 
-typedef struct page
-{
-   unsigned int present    : 1;   // Page present in memory
-   unsigned int rw         : 1;   // Read-only if clear, readwrite if set
-   unsigned int user       : 1;   // Supervisor level only if clear
-   unsigned int accessed   : 1;   // Has the page been accessed since last refresh?
-   unsigned int dirty      : 1;   // Has the page been written to since last refresh?
-   unsigned int unused     : 7;   // Amalgamation of unused and reserved bits
-   unsigned int frame      : 20;  // Frame address (shifted right 12 bits)
+/* Frame buffer supported color value */
+#define FB_BLACK 0
+#define FB_BLUE 1
+#define FB_GREEN 2
+#define FB_CYAN 3
+#define FB_RED 4
+#define FB_MAGENTA 5
+#define FB_BROWN 6
+#define FB_LIGHT_GREY 7
+#define FB_DARK_GREY 8
+#define FB_LIGHT_BLUE 9
+#define FB_LIGHT_GREEN 10
+#define FB_LIGHT_CYAN 11
+#define FB_LIGHT_RED 12
+#define FB_LIGHT_MAGENTA 13
+#define FB_LIGHT_BROWN 14
+#define FB_WHITE 15
+
+typedef struct page {
+  /* Page is present in memory if set */
+  u32int present : 1;
+
+  /* Read-only if clear, readwrite if set */
+  u32int rw : 1;
+
+  /* Supervisor level only if clear */
+  u32int user : 1;
+
+  /* Page been accessed since last refresh if set */
+  u32int accessed : 1;
+
+  /* Page been written to since last refresh if set */
+  u32int dirty : 1;
+
+  /* Amalgamation of unused and reserved bits */
+  u32int unused : 7;
+
+  /* Frame address (shifted right 12 bits). Since the frames are aligned at 4kb
+   * least 12 bits are always zero.
+   */
+  u32int frame : 20;
 } page_t;
 
-typedef struct page_table
-{
-   page_t pages[1024] __attribute__((aligned(4096)));
+typedef struct page_table {
+  page_t pages[1024];
 } page_table_t;
 
-typedef struct page_directory
-{
-   /**
-      Array of pointers to pagetables.
-   **/
-   page_table_t *tables[1024];
-   /**
-      Array of pointers to the pagetables above, but gives their *physical*
-      location, for loading into the CR3 register.
-   **/
-   unsigned int tablesPhysical[1024];
-   /**
-      The physical address of tablesPhysical. This comes into play
-      when we get our kernel heap allocated and the directory
-      may be in a different location in virtual memory.
-   **/
-   unsigned int physicalAddr;
+typedef struct page_directory {
+  /*
+   * Array of pointers to pagetables.
+   */
+  page_table_t *tables[1024];
+
+  /*
+   * Array of pointers to the pagetables above, but gives their *physical*
+   * location, for loading into the CR3 register.
+   */
+  u32int tablesPhysical[1024];
+
+  /*
+   * The physical address of tablesPhysical. This comes into play when we get
+   * our kernel heap allocated and the directory may be in a different location
+   * in virtual memory.
+   */
+  u32int physicalAddr;
 } page_directory_t;
 
+/*
+ * Sets up the environment, page directories etc and enables paging.
+ */
+void init_paging(u32int kerNelPhysicalStart, u32int kernelPhysicalEnd);
 
+/*
+ * Causes the specified page directory to be loaded into the CR3 register.
+ */
+void switch_page_directory(page_directory_t *new);
 
-/**
-  Sets up the environment, page directories etc and
-  enables paging.
-**/
-void init_paging();
+/*
+ * Retrieves a pointer to the page required. If make == 1, if the page-table in
+ * which this page should reside isn't created, create it!
+ */
+page_t *get_page(u32int address, u8int make, page_directory_t *dir);
 
-void page_fault();
-
-#endif
+#endif /* INCLUDE_PAGING_H */
